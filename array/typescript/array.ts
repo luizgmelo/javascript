45b18f6636fa4de65542166ffe882f5bcdbbe662
Luiz Guilherme <luizgmelo64@gmail.com>
2023-09-26 12:14:34 -0300

    array with types

---
+++ array/typescript/array.ts
@@ -0,0 +1,11 @@
+let megacena: number[]; 
+// let megacena: Array<number>;
+megacena = [1, 2, 3, 4, 5];
+console.log(megacena);
+
+// let arr = [1, 2, 'String', true, 2.1];
+
+// let ids: (number | string)[];
+let ids: Array<number|string>;
+ids = [1, 2, '3', '4'];
+console.log(ids);
