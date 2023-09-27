dbe8ff0118c56aecdb24e197109500e60b413688
Luiz Guilherme <luizgmelo64@gmail.com>
2023-09-27 11:43:13 -0300

    change items property to private in Stack

--- stack/array-stack.js
+++ stack/array-stack.js
@@ -1,36 +1,25 @@
 class Stack {
-  constructor() {
-    this.items = []; // {1}
-  }
+  #items = [];
   push(element) {
-    this.items.push(element);
+    this.#items.push(element);
   }
   pop() {
-    return this.items.pop();
+    return this.#items.pop();
   }
   peek() {
-    return this.items[this.items.length - 1];
+    return this.#items[this.#items.length - 1];
   }
   isEmpty() {
-    return this.items.length === 0;
+    return this.#items.length === 0;
   }
   size() {
-    return this.items.length;
+    return this.#items.length;
   }
   clear() {
-    return this.items = [];
+    return this.#items = [];
+  }
+  toString() {
+    return this.#items.toString();
   }
 }
 
-const stack = new Stack();
-console.log(stack.isEmpty());
-stack.push(5);
-stack.push(8);
-console.log(stack.peek());
-stack.push(11);
-console.log(stack.size());
-console.log(stack.isEmpty());
-stack.push(15);
-stack.pop();
-stack.pop();
-console.log(stack.size());
