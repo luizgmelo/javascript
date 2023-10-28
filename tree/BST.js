0e009fc0df56790e64b7dd750b54c4ab9bd9aea8
Luiz Guilherme <luizgmelo64@gmail.com>
2023-10-28 15:37:38 -0300

    AVL Binary Tree balance and insert method

--- tree/BST.js
+++ tree/BST.js
@@ -159,3 +159,126 @@ tree.remove(6)
 tree.remove(5)
 tree.remove(15)
 tree.inOrderTraverse(printNode);
+
+const BalanceFactor = {
+  UNBALANCED_RIGHT: 1,
+  SLIGHTLY_UNBALANCED_RIGHT: 2,
+  BALANCED: 3,
+  SLIGHTLY_UNBALANCED_LEFT: 4,
+  UNBALANCED_LEFT: 5
+};
+
+class AVLTree extends BinarySearchTree {
+  constructor(compareFn = defaultCompare) {
+    super();
+    this.compareFn = compareFn;
+    this.root = null;
+  }
+  getNodeHeight(node) {
+    if (node == null) {
+      return -1;
+    }
+    return Math.max(
+      this.getNodeHeight(node.left), this.getNodeHeight(node.right)
+    ) + 1;
+  }
+  getBalanceFactor(node) {
+    const heightDifference = this.getNodeHeight(node.left) - this.getNodeHeight(node.right);
+    switch (heightDifference) {
+      case -2:
+        return BalanceFactor.UNBALANCED_RIGHT;
+      case -1:
+        return BalanceFactor.SLIGHTLY_UNBALANCED_RIGHT;
+      case 1:
+        return BalanceFactor.SLIGHTLY_UNBALANCED_LEFT;
+      case 2:
+        return BalanceFactor.UNBALANCED_LEFT;
+      default:
+        return BalanceFactor.BALANCED;
+    }
+  }
+  rotationLL(node) {
+    const tmp = node.left;
+    node.left = tmp.right;
+    tmp.right = node;
+    return tmp;
+  }
+  rotationRR(node) {
+    const tmp = node.right;
+    node.right = tmp.left;
+    tmp.left = node;
+    return tmp;
+  }
+  rotationLR(node) {
+    node.left = this.rotationRR(node.left);
+    return this.rotationLL(node);
+  }
+  rotationRL(node) {
+    node.right = this.rotationLL(node.right);
+    return this.rotationRR(node);
+  }
+
+  // insert node
+
+  insert(key) {
+    this.root = this.insertNode(this.root, key);
+  }
+  insertNode(node, key) {
+    if (node == null) {
+      return new Node(key);
+    } else if (this.compareFn(key, node.key) === Compare.LESS_THAN) {
+      node.left = this.insertNode(node.left, key);
+    } else if (this.compareFn(key, node.key) === Compare.BIGGER_THAN) {
+      node.right = this.insertNode(node.right, key);
+    } else {
+      return node;
+    }
+    // balance if needs
+    const balanceFactor = this.getBalanceFactor(node);
+    if (balanceFactor === BalanceFactor.UNBALANCED_LEFT) {
+      if (this.compareFn(key, node.left.key) === Compare.LESS_THAN) {
+        node = this.rotationLL(node);
+      } else {
+        return this.rotationLR(node);
+      }
+    }
+    if (balanceFactor === BalanceFactor.UNBALANCED_RIGHT) {
+      if (
+        this.compareFn(key, node.right.key) === Compare.BIGGER_THAN 
+      ) {
+        node = this.rotationRR(node);
+      } else {
+        return this.rotationRL(node);
+      }
+    }
+    return node;
+  }
+}
+
+console.log("AVL")
+const avl = new AVLTree();
+// // balance LL Left-left: simple right rotation
+// avl.insert(3);
+// avl.insert(2);
+// avl.insert(1);
+// avl.preOrderTraverse(printNode);
+
+// // balance RR: simple left rotation
+// avl.insert(1);
+// avl.insert(2);
+// avl.insert(3);
+// avl.preOrderTraverse(printNode);
+
+// // balance LR: double rotation for right
+// avl.insert(3);
+// avl.insert(1);
+// avl.insert(2);
+// avl.preOrderTraverse(printNode)
+
+// // balanceRL: double rotation for left
+// avl.insert(1);
+// avl.insert(3);
+// avl.insert(2);
+// avl.preOrderTraverse(printNode)
+
+// work fine!
